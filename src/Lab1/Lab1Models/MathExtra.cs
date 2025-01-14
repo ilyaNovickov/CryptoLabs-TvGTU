0c3056025797c76f187ccccdf3d0ac6775ef007d
Ilya Novikov <ilyanovickov@mail.ru>
2025-01-14 18:02:34 +0300

    Ввод класса для математики

---
+++ src/Lab1/Lab1Models/MathExtra.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Models
+{
+    internal static class MathExtra
+    {
+        /// <summary>
+        /// Быстрое возведение в степень по модулю.
+        /// Вычисляет (base^exp) % mod.
+        /// </summary>
+        /// <param name="baseValue">Основание (a).</param>
+        /// <param name="exponent">Показатель степени (x).</param>
+        /// <param name="modulus">Модуль (y).</param>
+        /// <returns>Результат вычислений (a^x % y).</returns>
+        public static ulong ModularExponentiation(ulong baseValue, ulong exponent, ulong modulus)
+        {
+            /*
+             * Спасибо ChatGPT за разъяснение.
+             * 
+             * Идея испольует свойства mod и степеней:
+             * - (a*b) mod y = [ (a mod y) * (b mod y) ] mod y
+             * - a^x * a^y = a^(x+y)
+             * а также в том, что любое число можно представить
+             * как степень двойки:
+             * 10 = 0b1010
+             * Вспоминаем, как переводить числа из двоичной системы 
+             * в десятичную:
+             * 0b1010 => 2^3 * 1 + 2^2 * 0 + 2^1 * 1 + 2^0 * 0 = 2^3 * 2^1 = 8 + 2 = 10
+             * 
+             * Таким образом возведение a^10 - это a^[ 2^3 + 2^1 ]
+             * Значит "a^10 mod y" => "(a^2^3 * a^2^1) mod y " =>
+             * => "[ (a^2^3 mod y) * (a^2^1 mod y) ] mod y"
+             * 
+             * Всё это в конечном итоге развернётся в:
+             * [...[[a mod y * a mod y] * mod y ] * ... ] 
+             */
+            if (modulus == 1)
+                return 0; // Если mod = 1, то результат всегда 0
+
+            ulong result = 1;          // Начальное значение результата
+            //По факрту: a mod y
+            ulong baseMod = baseValue % modulus; // Приводим основание по модулю
+
+            while (exponent > 0)
+            {
+                // Если текущая степень нечётная, умножаем результат на основание
+                //Нас интирисует только нечётные числа, 
+                //которые заканчиваются на "1" в bin
+                if ((exponent & 1) == 1)
+                {
+                    result = (result * baseMod) % modulus;
+                }
+
+                // Возводим основание в квадрат, уменьшив степень вдвое
+                //По факту: [ (a mod y) * (a mod y) ] mod y на I-ой итерации
+                //т. е. a^2 mod y
+                //На отсальных итерациях будет a^4 mod y, a^6 mod y и т. д.
+                baseMod = (baseMod * baseMod) % modulus;
+
+                // Переходим к следующему разряду экспоненты
+                exponent >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
